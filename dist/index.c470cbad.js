cc4fca4fab9de8b97ea66786416345e5d4e2434c
ujunghee <zmfozj4172@naver.com>
2024-10-16 17:50:58 +0900

    메세지

--- dist/index.c470cbad.js
+++ dist/index.c470cbad.js
@@ -1,16 +1,21 @@
+// 네비게이션 바 요소 이벤트
 function navigation() {
     let activeToggle = null;
     let popup = document.querySelector(".object");
     let colorPalette = document.querySelector(".colorpalette");
     let textField = document.querySelector(".textfield");
+    let infortMore = document.querySelector(".infor");
     // 해당 클릭할 요소 선택 및 공통 이벤트 실행
     document.addEventListener("click", function(event) {
-        let clickedToggle = event.target.closest(".p-b, .painting, .textfielding");
         // 변수에 할당된 요소들이 클릭할 때 handleToggle 호출
+        let clickedToggle = event.target.closest(".p-b, .painting, .textfielding");
         if (clickedToggle) handleToggle(clickedToggle);
         // scroll 요소 클릭할 때 closeAllPopups 호출
         let scrollElement = event.target.closest(".scroll");
         if (scrollElement) closeAllPopups();
+        // 가구 자세히보기
+        if (event.target.closest(".f-button")) infortMore.classList.add("on");
+        if (event.target.closest(".close")) infortMore.classList.remove("on");
     });
     //  토글 활성 비활성화
     function handleToggle(toggle) {
--- js/deco.js
+++ js/deco.js
@@ -1,14 +1,16 @@
+// 네비게이션 바 요소 이벤트
 function navigation() {
     let activeToggle = null;
     let popup = document.querySelector('.object');
     let colorPalette = document.querySelector('.colorpalette');
     let textField = document.querySelector('.textfield');
+    let infortMore = document.querySelector('.infor');
 
     // 해당 클릭할 요소 선택 및 공통 이벤트 실행
     document.addEventListener('click', function (event) {
-        let clickedToggle = event.target.closest('.p-b, .painting, .textfielding');
 
         // 변수에 할당된 요소들이 클릭할 때 handleToggle 호출
+        let clickedToggle = event.target.closest('.p-b, .painting, .textfielding');
         if (clickedToggle) {
             handleToggle(clickedToggle);
         }
@@ -17,6 +19,14 @@ function navigation() {
         let scrollElement = event.target.closest('.scroll');
         if (scrollElement) {
             closeAllPopups();
+        }
+
+        // 가구 자세히보기
+        if(event.target.closest('.f-button')) {
+            infortMore.classList.add('on');
+        }
+        if(event.target.closest('.close')) {
+            infortMore.classList.remove('on');
         }
     });
 
@@ -65,4 +75,6 @@ function navigation() {
         document.querySelectorAll('.p-b, .painting, .textfielding').forEach(toggle => toggle.classList.remove('on'));
         activeToggle = null;
     }
+
+
 }

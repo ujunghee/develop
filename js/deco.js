0f29622d8c2d728e53fcfd16f0191fb4e1723af7
ujunghee <zmfozj4172@naver.com>
2024-10-22 15:15:00 +0900

    데이터 추가

--- js/deco.js
+++ js/deco.js
@@ -84,6 +84,8 @@ function navigation() {
 function objectItem() {
     const popupImages = document.querySelectorAll('.piclist li img');
 
+
+    // 가구, 도자기, 책 이미지
     const imgPaths = {
         furniture: [
             "./image/object/furniture/display-01.png",
@@ -118,9 +120,56 @@ function objectItem() {
             "./image/object/book/display-27.png",
             "./image/object/book/display-28.png",
             "./image/object/book/display-29.png",
-        ]
+        ],
+        season: {
+            spring: [
+                "./image/object/season/spring/display-38.png",
+                "./image/object/season/spring/display-39.png",
+                "./image/object/season/spring/display-40.png",
+                "./image/object/season/spring/display-41.png",
+                "./image/object/season/spring/display-42.png",
+                "./image/object/season/spring/display-43.png",
+                "./image/object/season/spring/display-44.png",
+                "./image/object/season/spring/display-45.png",
+                "./image/object/season/spring/display-46.png",
+            ],
+            summer: [
+                "./image/object/season/summer/display-47.png",
+                "./image/object/season/summer/display-48.png",
+                "./image/object/season/summer/display-49.png",
+                "./image/object/season/summer/display-50.png",
+                "./image/object/season/summer/display-51.png",
+                "./image/object/season/summer/display-52.png",
+                "./image/object/season/summer/display-53.png",
+                "./image/object/season/summer/display-54.png",
+                "./image/object/season/summer/display-55.png",
+            ],
+            autumn: [
+                "./image/object/season/autumn/display-65.png",
+                "./image/object/season/autumn/display-66.png",
+                "./image/object/season/autumn/display-67.png",
+                "./image/object/season/autumn/display-68.png",
+                "./image/object/season/autumn/display-69.png",
+                "./image/object/season/autumn/display-70.png",
+                "./image/object/season/autumn/display-71.png",
+                "./image/object/season/autumn/display-72.png",
+                "./image/object/season/autumn/display-73.png",
+            ],
+            winter: [
+                "./image/object/season/winter/display-56.png",
+                "./image/object/season/winter/display-57.png",
+                "./image/object/season/winter/display-58.png",
+                "./image/object/season/winter/display-59.png",
+                "./image/object/season/winter/display-60.png",
+                "./image/object/season/winter/display-61.png",
+                "./image/object/season/winter/display-62.png",
+                "./image/object/season/winter/display-63.png",
+                "./image/object/season/winter/display-64.png",
+            ]
+        }
     }
 
+    // 가구, 도자기, 책 텍스트
     const altPaths = {
         furniture: [
             "소반",
@@ -154,57 +203,54 @@ function objectItem() {
             "봄무늬 서책",
             "여름무늬 서책",
             "펼처진 서책",
-        ]
+        ],
+        season: {
+            spring: [
+                "복숭아",
+                "모란꽃",
+                "앵화",
+                "호랑이·까치",
+                "모란·화병",
+                "서각통",
+                "나비1",
+                "나비2",
+                "나비3",
+            ],
+            summer: [
+                "물고기",
+                "거북이",
+                "소나무",
+                "석류·석류꽃",
+                "연꽃·수반",
+                "대나무",
+                "영지버섯",
+                "수박",
+                "고양이",
+            ],
+            autumn: [
+                "호박",
+                "수탉",
+                "감",
+                "불수감",
+                "공작새 깃털",
+                "국화",
+                "접시꽃",
+                "어치",
+                "단풍",
+            ],
+            winter: [
+                "향로·토끼",
+                "옥토끼",
+                "조롱박",
+                "매화",
+                "필통",
+                "파초잎",
+                "벼루",
+                "치자꽃",
+                "바둑",
+            ],
+        }
     }
-
-    // 시즌
-    const seasonConfig = {
-        spring: [
-            "./image/object/season/spring/display-38.png",
-            "./image/object/season/spring/display-39.png",
-            "./image/object/season/spring/display-40.png",
-            "./image/object/season/spring/display-41.png",
-            "./image/object/season/spring/display-42.png",
-            "./image/object/season/spring/display-43.png",
-            "./image/object/season/spring/display-44.png",
-            "./image/object/season/spring/display-45.png",
-            "./image/object/season/spring/display-46.png",
-        ],
-        summer: [
-            "./image/object/season/summer/display-47.png",
-            "./image/object/season/summer/display-48.png",
-            "./image/object/season/summer/display-49.png",
-            "./image/object/season/summer/display-50.png",
-            "./image/object/season/summer/display-51.png",
-            "./image/object/season/summer/display-52.png",
-            "./image/object/season/summer/display-53.png",
-            "./image/object/season/summer/display-54.png",
-            "./image/object/season/summer/display-55.png",
-        ],
-        autumn: [
-            "./image/object/season/autumn/display-65.png",
-            "./image/object/season/autumn/display-66.png",
-            "./image/object/season/autumn/display-67.png",
-            "./image/object/season/autumn/display-68.png",
-            "./image/object/season/autumn/display-69.png",
-            "./image/object/season/autumn/display-70.png",
-            "./image/object/season/autumn/display-71.png",
-            "./image/object/season/autumn/display-72.png",
-            "./image/object/season/autumn/display-73.png",
-        ],
-        winter: [
-            "./image/object/season/winter/display-56.png",
-            "./image/object/season/winter/display-57.png",
-            "./image/object/season/winter/display-58.png",
-            "./image/object/season/winter/display-59.png",
-            "./image/object/season/winter/display-60.png",
-            "./image/object/season/winter/display-61.png",
-            "./image/object/season/winter/display-62.png",
-            "./image/object/season/winter/display-63.png",
-            "./image/object/season/winter/display-64.png",
-        ]
-    };
-
     const seasonAlt = {
         spring: [
             "젊음과 청춘, 아름다움",
@@ -218,37 +264,37 @@ function objectItem() {
             "변화와 불멸, 기쁨",
         ],
         summer: [
-            "",
-            "",
-            "",
-            "",
-            "",
-            "",
-            "",
-            "",
-            "",
+            "번영,역동성,벽사",
+            "장수의 상징",
+            "불로장생",
+            "유월, 자식번성, 번창, 풍요",
+            "칠월, 여름, 아름다움",
+            "장수를 축하하고 기원",
+            "불로장생",
+            "다산, 장수, 풍요",
+            "장수, 악귀를 쫓는",
         ],
         autumn: [
-            "",
-            "",
-            "",
-            "",
-            "",
-            "",
-            "",
-            "",
-            "",
+            "복과 풍요",
+            "출세와 성공",
+            "번창, 벼슬이 높아짐",
+            "부, 신의 가호, 복의상징",
+            "벼슬, 관직",
+            "시월, 가을, 품위",
+            "구월, 승진",
+            "관운, 승진",
+            "대궐, 조정",
         ],
         winter: [
-            "",
-            "",
-            "",
-            "",
-            "",
-            "",
-            "",
-            "",
-            "",
+            "학문, 세상만물, 명예, 지혜",
+            "지혜, 부활, 불멸",
+            "길상, 선비, 액운 물리침",
+            "선비, 겨울, 선비의 기품",
+            "학문의 성취",
+            "독학, 신선, 기사회생, 부귀",
+            "학문",
+            "신중함, 11월",
+            "학자의 표상",
         ],
     }
 
@@ -274,6 +320,7 @@ function objectItem() {
 
     // 이벤트 리스너 함수를 분리하여 관리
     document.addEventListener('click', handleNavClick);
+    let seasonValue = 'spring';
 
     function handleNavClick(e) {
         const navElement = e.target.closest('.p-b');
@@ -281,9 +328,9 @@ function objectItem() {
 
         const fButton = document.querySelector('.f-button');
         const fTwist = document.querySelector('.f-twist');
-        
+
         // 현재 클릭된 요소의 카테고리 찾기
-        const category = Object.keys(categoryConfig).find(key => 
+        const category = Object.keys(categoryConfig).find(key =>
             navElement.classList.contains(key)
         );
 
@@ -292,6 +339,14 @@ function objectItem() {
             return;
         }
 
+        // 계절 버튼 클릭시 seasonValue 업데이트
+        if (category === 'season') {
+            const clickedSeason = navElement.dataset.season || navElement.value;
+            if (clickedSeason) {
+                seasonValue = clickedSeason;
+            }
+        }
+        
         // 버튼 상태 업데이트
         const { buttonStates } = categoryConfig[category];
         fButton.classList.toggle('on', buttonStates.fButton);
@@ -300,23 +355,35 @@ function objectItem() {
 
         // 이미지 업데이트
         updateImages(category);
-
-        // 좌우반전
-
     }
 
+
+    // 버튼 클릭 이벤트
     function updateImages(category) {
+
         if (!imgPaths[category]) {
             console.warn(`${category}에 대한 이미지 경로가 없습니다.`);
             return;
         }
 
         popupImages.forEach((img, index) => {
-            if (index < imgPaths[category].length) {
-                img.src = imgPaths[category][index];
-                img.alt = altPaths[category][index];
-            } 
-            // else if()
+            if (category === 'season') {
+                if (index < imgPaths.season[seasonValue].length) {
+                    img.src = imgPaths.season[seasonValue][index];
+                    img.alt = altPaths.season[seasonValue][index];
+                } else {
+                    img.src = '';
+                    img.alt = '';
+                }
+            } else {
+                if (index < imgPaths[category].length) {
+                    img.src = imgPaths[category][index];
+                    img.alt = altPaths[category][index];
+                } else {
+                    img.src = '';
+                    img.alt = '';
+                }
+            }
         });
     }
 }

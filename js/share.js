d7634f5b30848cf352584b38fb1c6b20d8f51197
ujunghee <zmfozj4172@naver.com>
2024-11-07 10:33:46 +0900

    js

---
+++ js/share.js
@@ -0,0 +1,26 @@
+function share() {
+    const shareData = {
+        title: "민화꾸미기",
+        text: "사계절에 맞는 민화를 꾸며보세요",
+        url: "https://ujunghee.github.io/develop/"
+    };
+
+    const shareButton = document.querySelector('.share');
+    const resultElement = document.querySelector('.result');
+
+    // 클릭 이벤트 리스너 추가
+    shareButton.addEventListener("click", async () => {
+        try {
+            // Web Share API 실행
+            await navigator.share(shareData);
+            if (resultElement) {
+                resultElement.textContent = "공유가 완료되었습니다!";
+            }
+        } catch (err) {
+            if (resultElement) {
+                resultElement.textContent = `공유 중 오류가 발생했습니다: ${err}`;
+            }
+            console.error("공유 오류:", err);
+        }
+    });
+}
